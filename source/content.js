229ef5516f062994e6471c9510b76bf0a6398c84
Mathieu LEMAIRE <mathieu@getvega.com>
2011-02-09 19:57:26 +0100

    fix icon disappearing + search by splitting request terms (AND operator)

--- source/content.js
+++ source/content.js
@@ -4,11 +4,14 @@ chrome.extension.sendRequest({action:"pageStart"});
 
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
+  		
+  		var words = request.query.toLowerCase().split(" ");
+
   		var results = new Array();
   		vega_$(".item").each(function(){
   			if (this.id.indexOf("_story") != -1) {
   				var name = vega_$(this).find(".storyPreviewText > span:nth-child(2)").html();
-	  			if (name.toLowerCase().indexOf(request.query.toLowerCase()) != -1) {
+	  			if (pivothub_matches(name, words)) {
   					var id = this.id;
 	  				var s = id.lastIndexOf("story");
   					storyId = id.substr(s + 5, id.length - 5 - s);
@@ -18,3 +21,13 @@ chrome.extension.onRequest.addListener(
   		});
 		sendResponse(results);
   });
+
+var pivothub_matches = function(name, words) 
+{
+	var lName = name.toLowerCase();
+	var found = true;
+	vega_$(words).each(function(){
+		if (lName.indexOf(this) == -1) found = false;
+	});
+	return found;
+};
